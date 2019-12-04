322c4a9ce75bb786027416af074b21aa73da4e0b
Wagner Filgueiras <38699596+wagner-filgueiras@users.noreply.github.com>
2019-12-04 15:24:18 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src';
+import {name as myProject2} from './app.json';
+
+AppRegistry.registerComponent(myProject2, () => App);
